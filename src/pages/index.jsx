b635db3dbd3cdc51dcbc69433e0839c646af6634
YilutMak <Yilutmak@gmail.com>
2023-03-08 00:02:30 +0800

    added project 1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,14 +17,35 @@ export default function Home() {
       <div id="home" className="scroll-item d-flex justify-content-center align-items-center" style={{ backgroundColor: 'gray' }}>
 
         <div className="">
-          <h1 id="intro" className="row">Hi,I'm Cytus Mak,<br /> Full Stack Developer</h1>
+          <h1 id="intro" className="row" style={{ fontFamily: 'Courier New' }}>Hi,I'm Cytus Mak,<br /> Full Stack Developer</h1>
 
-          <div id="browseProject" className="row justify-content-center">Browse Projects</div>
+          <div id="browseProject" className="row"><a
+            href="#project1"
+            id="button"
+            className="row justify-content-center"
+            style={{
+              textDecoration: 'none',
+              color: 'white'
+            }}
+          >Browse Projects</a></div>
+
         </div>
 
       </div>
 
-      <div id="project1" className="scroll-item" style={{ backgroundColor: 'red' }} />
+      <div id="project1" className="scroll-item d-flex justify-content-end " style={{ backgroundColor: 'red' }}>
+        <div id="section1" className="">
+          <div className="" style={{ marginTop: '35vh', marginRight: '4vh' }}>
+            <h1 style={{ fontSize: '7.5vh', lineHeight: '.7' }}>Game</h1>
+            <h2 style={{ fontSize: '3.5vh', marginLeft: '5px' }}>Resident Pixel 2</h2>
+            <h5 style={{ fontSize: '2vh', marginTop: '15px', marginLeft: '5px' }}>Recreating a simple 2D top-down view shooter with incremental difficulty and immersiveness</h5>
+            <button id="pressButton" type="button" className="btn btn-light" style={{ marginTop: '1vh', fontSize: '18px', fontWeight: 'bold' }}>Play</button>
+            <div id="scrolldown">
+              <a id="scrollDownWord" href="#project2">Scroll Down</a>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div id="project2" className="scroll-item" style={{ backgroundColor: 'green' }} />
 
